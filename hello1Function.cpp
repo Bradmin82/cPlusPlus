2da2b3e6eb5460dacc188ad7dee80ea07df7d0af
kosherroach <68676875+kosherroach@users.noreply.github.com>
2020-08-26 18:03:31 -0400

    Create hello1Function.cpp

---
+++ hello1Function.cpp
@@ -0,0 +1,13 @@
+/*
+* hello1Function.cpp
+*
+*/ 
+
+#include <iostream>
+
+int hello1Function(string name1)
+{
+cout << "hello World << name1 << end?
+cout << "Hey " << name1 << " , North America " << endl;
+return 0;
+}
